4e3708752fd9bfd8d64aaef33d0f029d4769d32d
Dărăbăneanu Cosmin <60667045+DarabaneanuCosmin@users.noreply.github.com>
2021-11-06 09:26:23 -0700

    v1

---
+++ WebAPI/Controllers/v1/UserDataController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Entities;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiController]
+    public class UserDataController: BaseController
+    {
+        [HttpPost]
+        public string Post()
+        {
+            return "hi";
+        }
+
+        [HttpGet]
+        public IEnumerable<DataNews> Get(int id)
+        {
+            return Enumerable.Range(1, 1).Select(index => new DataNews
+            {
+                Title = "As U.S. budget fight looms, Republicans flip their fiscal script",
+                Text = "WASHINGTON (Reuters) - The head of a conservative Republican faction in the U.S. Congress, who voted...",
+                Subject = "politicsNews",
+                Date = new DateTime(2017,12,31)
+            }).ToArray();
+        }
+    }
+}
