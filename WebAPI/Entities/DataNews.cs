c40097f04f5ca6b4489b6a8b720f53887a759497
Dărăbăneanu Cosmin <60667045+DarabaneanuCosmin@users.noreply.github.com>
2021-11-06 08:10:02 -0700

    [Back-end-v1]two endpoints

---
+++ WebAPI/Entities/DataNews.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebAPI.Entities
+{
+    public class DataNews
+    {
+        public string Title { get; set; }
+        
+        public string Text { get; set; }
+        
+        public string Subject { get; set; }
+        
+        public DateTime Date { get; set; }
+    }
+}
