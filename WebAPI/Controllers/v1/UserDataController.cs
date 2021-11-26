a92acafba3cdc9c28f5107fd8fd4ad00340db8b4
Dărăbăneanu Cosmin <60667045+DarabaneanuCosmin@users.noreply.github.com>
2021-11-25 23:18:33 -0800

    db connection

--- WebAPI/Controllers/v1/UserDataController.cs
+++ WebAPI/Controllers/v1/UserDataController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 using WebAPI.Entities;
 
 namespace WebAPI.Controllers.v1
@@ -12,6 +15,8 @@ namespace WebAPI.Controllers.v1
     [ApiController]
     public class UserDataController: BaseController
     {
+        public static IConfiguration AppSetting { get; }
+
         [HttpPost]
         public HttpResponseMessage Post()
         {
@@ -19,10 +24,12 @@ namespace WebAPI.Controllers.v1
         }
 
         [HttpGet]
-        public IEnumerable<Article> Get(int id)
+        public async Task<IEnumerable<Article>> GetAsync(int id)
         {
-            return Enumerable.Range(1, 1).Select(index => new Article
+           
+           return Enumerable.Range(1, 1).Select(index => new Article
             {
+
                 Title = "As U.S. budget fight looms, Republicans flip their fiscal script",
                 Text = "WASHINGTON (Reuters) - The head of a conservative Republican faction in the U.S. Congress, who voted...",
                 Subject = "politicsNews",
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
+
 namespace WebAPI
 {
     public class Startup
@@ -28,6 +30,7 @@ namespace WebAPI
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPI", Version = "v1"}); });
             services.AddApiVersioning();
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
