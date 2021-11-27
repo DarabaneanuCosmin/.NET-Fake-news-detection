304661215cd467a8e4c13368a7d4c0ea26d83580
craciunmarian <razvanmariancr@gmail.com>
2021-11-27 20:00:29 +0200

    headers

--- fake-new-front-end/src/app/components/header/header.component.ts
+++ fake-new-front-end/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IUser } from 'src/app/final-pages/user-page/IUser';
 
 @Component({
@@ -9,15 +9,28 @@ import { IUser } from 'src/app/final-pages/user-page/IUser';
 })
 export class HeaderComponent implements OnInit {
 
-  //private URL = 'https://localhost:4200/api/v1/UserAuthentification?email=darabaneanucosmin81@gmail.com';
-  private URL = "/assets/user.json";
+  private URL = 'https://localhost:5001/api/v1/UserAuthentification?email=darabaneanucosmin81@gmail.com';
+  //private URL = "/assets/user.json";
   request: any[];
+
+  private headerDict = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin':  'https://localhost:5001/api/v1/',
+  }
+
+  private requestOptions = {                                                                                                                                                                                 
+    headers: new HttpHeaders(this.headerDict), 
+  };
+  
 
 
   constructor(private http:HttpClient) {
-    this.http.get<IUser[]>(this.URL).subscribe(
+    this.http.get<IUser[]>(this.URL, this.requestOptions).subscribe(
       data =>{
         this.request = data;
+        console.log(data);
       },
       () => {},
       () => this.update()
