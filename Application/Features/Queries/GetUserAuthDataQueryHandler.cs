fc2c5fe6b1b21fa8e0c30f7795823b4d6a256c61
Dărăbăneanu Cosmin <60667045+DarabaneanuCosmin@users.noreply.github.com>
2021-12-02 11:29:47 -0800

    fix

--- Application/Features/Queries/GetUserAuthDataQueryHandler.cs
+++ Application/Features/Queries/GetUserAuthDataQueryHandler.cs
@@ -26,9 +26,9 @@ namespace WebAPI.Features.Queries
                 this.connection = new MySqlConnection(this.connectionString);
                 connection.OpenAsync();
 
-                using var command = new MySqlCommand("SELECT * FROM user WHERE user.email_address = " + "@email_address" + " AND user.pass = " + "@encryptedPassword", this.connection);
+                using var command = new MySqlCommand("SELECT * FROM user WHERE user.email_address = " + "@email_address" + " AND user.password = " + "@EncryptedPassword", this.connection);
                 command.Parameters.AddWithValue("@email_address", user.email_address);
-                command.Parameters.AddWithValue("@encryptedPassword", Security.EncryptString(user.encryptedPassword));
+                command.Parameters.AddWithValue("@EncryptedPassword", Security.EncryptString(user.encryptedPassword));
 
                 MySqlDataReader rdr = command.ExecuteReader();
                 while (rdr.Read())
