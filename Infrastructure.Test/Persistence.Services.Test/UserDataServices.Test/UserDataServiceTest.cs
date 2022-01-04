018ca538f0925c275f9fead20be768cb747f5117
ctlnlzr <lazarcatalina377@gmail.com>
2022-01-04 12:38:56 +0200

    UT

---
+++ Infrastructure.Test/Persistence.Services.Test/UserDataServices.Test/UserDataServiceTest.cs
@@ -0,0 +1,113 @@
+﻿using Application.Features.Queries;
+using WebAPI.Features.Commands;
+using WebAPI.Features.Queries;
+using WebAPI.Services;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+using WebAPI.Responses;
+
+namespace Infrastructure.Test.Persistence.Services.Test.Persistence.Services.UserDataServices.Test
+{
+    public class UserDataServiceTest
+    {
+        [Fact]
+        public void AfterInitializationAllFieldsShouldNotBeNull()
+        {
+            //arrange 
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            InsertArticleCommandHandler insertArticleCommandHandler = new InsertArticleCommandHandler(configuration);
+            IsUserWithIdQueryHandler isUserWithIdQueryHandler = new IsUserWithIdQueryHandler(configuration);
+            GetUserIdByUserTokenHandler getUserIdByUserTokenHandler = new GetUserIdByUserTokenHandler(configuration);
+            GetArticlesByUserIdQueryHandler GetArticlesByUserIdQueryHandler = new GetArticlesByUserIdQueryHandler(configuration);
+            
+            //act
+            UserDataService user = new UserDataService(insertArticleCommandHandler, isUserWithIdQueryHandler, getUserIdByUserTokenHandler, GetArticlesByUserIdQueryHandler);
+
+            
+            //Assert
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public void InsertTest_ExpectError()
+        {
+            //arrange 
+            InsertArticleUsingTokenCommand insertArticleUsingTokenCommand = new InsertArticleUsingTokenCommand();
+            insertArticleUsingTokenCommand.Token = "1234";
+            insertArticleUsingTokenCommand.Subject = "test";
+            insertArticleUsingTokenCommand.Text = "text";
+            insertArticleUsingTokenCommand.Title = "titlu";
+            insertArticleUsingTokenCommand.Date_article = "10/10/2010";
+            insertArticleUsingTokenCommand.Is_fake = true;
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            InsertArticleCommandHandler insertArticleCommandHandler = new InsertArticleCommandHandler(configuration);
+            IsUserWithIdQueryHandler isUserWithIdQueryHandler = new IsUserWithIdQueryHandler(configuration);
+            GetUserIdByUserTokenHandler getUserIdByUserTokenHandler = new GetUserIdByUserTokenHandler(configuration);
+            GetArticlesByUserIdQueryHandler GetArticlesByUserIdQueryHandler = new GetArticlesByUserIdQueryHandler(configuration);
+            UserDataService user = new UserDataService(insertArticleCommandHandler, isUserWithIdQueryHandler, getUserIdByUserTokenHandler, GetArticlesByUserIdQueryHandler);
+
+
+            //act
+            InsertArticleResponse response = user.Insert(insertArticleUsingTokenCommand);
+
+            //Assert
+            Assert.True(response.Error);
+        }
+
+        [Fact]
+        public void InsertTest_DoesntExpectError()
+        {
+            //arrange 
+            InsertArticleUsingTokenCommand insertArticleUsingTokenCommand = new InsertArticleUsingTokenCommand();
+            insertArticleUsingTokenCommand.Token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzkxMzM1NjEsImNsYWltMiI6ImNsYWltMi12YWx1ZSJ9.BVIYs3MPQ7uhOnbNpOZK_fGtmDUPgtZVa44lo0mXPy0";
+            insertArticleUsingTokenCommand.Subject = "test";
+            insertArticleUsingTokenCommand.Text = "text";
+            insertArticleUsingTokenCommand.Title = "titlu";
+            insertArticleUsingTokenCommand.Date_article = "10/10/2010";
+            insertArticleUsingTokenCommand.Is_fake = true;
+            var configuration = new ConfigurationBuilder().AddConfiguration().Build();
+            InsertArticleCommandHandler insertArticleCommandHandler = new InsertArticleCommandHandler(configuration);
+            IsUserWithIdQueryHandler isUserWithIdQueryHandler = new IsUserWithIdQueryHandler(configuration);
+            GetUserIdByUserTokenHandler getUserIdByUserTokenHandler = new GetUserIdByUserTokenHandler(configuration);
+            GetArticlesByUserIdQueryHandler GetArticlesByUserIdQueryHandler = new GetArticlesByUserIdQueryHandler(configuration);
+            UserDataService user = new UserDataService(insertArticleCommandHandler, isUserWithIdQueryHandler, getUserIdByUserTokenHandler, GetArticlesByUserIdQueryHandler);
+
+
+            //act
+            InsertArticleResponse response = user.Insert(insertArticleUsingTokenCommand);
+
+            //Assert
+            Assert.False(response.Error);
+        }
+
+        [Fact]
+        public void GetArticles_ExpectEmptyCollection()
+        {
+            //arrange 
+            var configuration = new ConfigurationBuilder().Build();
+            InsertArticleCommandHandler insertArticleCommandHandler = new InsertArticleCommandHandler(configuration);
+            IsUserWithIdQueryHandler isUserWithIdQueryHandler = new IsUserWithIdQueryHandler(configuration);
+            GetUserIdByUserTokenHandler getUserIdByUserTokenHandler = new GetUserIdByUserTokenHandler(configuration);
+            GetArticlesByUserIdQueryHandler GetArticlesByUserIdQueryHandler = new GetArticlesByUserIdQueryHandler(configuration);
+            UserDataService user = new UserDataService(insertArticleCommandHandler, isUserWithIdQueryHandler, getUserIdByUserTokenHandler, GetArticlesByUserIdQueryHandler);
+
+
+            //act
+            GetArticlesResponse response = user.GetArticles("1234");
+
+            //Assert
+            Assert.Empty(response.Articles);
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
